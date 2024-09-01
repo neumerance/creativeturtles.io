f5e45136fb3b0973233577f2af2fc60f1de0a8eb
JonJon <neumerance@live.com>
2024-09-02 01:37:34 +0800

    reject if no current_user

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -11,6 +11,8 @@ module ApplicationCable
       decoded_token = JWT.decode token, ENV["AUTH_SECRET"], true, { algorithm: "HS256" }
       identifiers = JSON.parse(decoded_token.first["ext"])
     
+      reject_unauthorized_connection unless identifiers.dig("current_user", "id").present?
+
       self.current_user = User.find(identifiers.dig("current_user", "id"))
     rescue JWT::DecodeError => e
       Rails.logger.error "ERROR: #{e.to_json}"
